987c17ef6009cdd4fe14cf4836196aff82076ba2
Sultan Iman <sultan.imanhodjaev@gmail.com>
2026-05-03 20:55:14 +0200

    add source export condition, simplify demo vite config

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@wetron/react": resolve(__dirname, "../../packages/react/src/index.ts"),
-    },
+    conditions: ["source"],
   },
   server: {
     watch: {
-      ignored: ["**/packages/*/test/**"],
+      ignored: ["**/packages/*/test/**", "**/packages/*/dist/**"],
     },
   },
 });
